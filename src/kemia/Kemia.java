dd33246f24f33e21563b88a2862552a192be20b2
VBalazs <v.vbalazs@gmail.com>
2021-03-04 10:52:44 +0100

    3. feladat

--- src/kemia/Kemia.java
+++ src/kemia/Kemia.java
@@ -13,6 +13,14 @@ public class Kemia {
         Path path = Paths.get("felfedezesek.csv");
         List<String> sorok = Files.readAllLines(path);
         List<KemiaiElem> elemek = new ArrayList<>();
+        
+        for (int i = 1; i < sorok.size(); i++) {
+            elemek.add(new KemiaiElem(sorok.get(i)));
+        }
+        
+        System.out.println("Elemek száma: "+elemek.size());
+        
+        
     }
     
 }
--- src/kemia/KemiaiElem.java
+++ src/kemia/KemiaiElem.java
@@ -8,6 +8,16 @@ public class KemiaiElem {
     private int rendszam;
     private String felfedezo;
 
+    public KemiaiElem(String sor) {
+        String [] s= sor.split(";");
+        this.ev = s[0];
+        this.elem = s[1];
+        this.vegyjel = s[2];
+        this.rendszam = Integer.parseInt(s[3]);
+        this.felfedezo = s[4];
+    }
+    
+    
     @Override
     public String toString() {
         return "KemiaiElem{" + "ev=" + ev + ", elem=" + elem + ", vegyjel=" + vegyjel + ", rendszam=" + rendszam + ", felfedezo=" + felfedezo + '}';
@@ -33,12 +43,5 @@ public class KemiaiElem {
         return felfedezo;
     }
 
-    public KemiaiElem(String sor) {
-        String [] s= sor.split(";");
-        this.ev = s[0];
-        this.elem = s[1];
-        this.vegyjel = s[2];
-        this.rendszam = Integer.parseInt(s[3]);
-        this.felfedezo = s[4];
-    }
+    
 }
