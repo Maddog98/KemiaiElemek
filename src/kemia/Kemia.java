aec7a27c553e9f2bb7f61224b666f4c8068e945a
VBalazs <v.vbalazs@gmail.com>
2021-03-04 10:43:04 +0100

    2. feladat

--- src/kemia/Kemia.java
+++ src/kemia/Kemia.java
@@ -1,9 +1,18 @@
 package kemia;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kemia {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("felfedezesek.csv");
+        List<String> sorok = Files.readAllLines(path);
+        List<KemiaiElem> elemek = new ArrayList<>();
     }
     
 }
---
+++ src/kemia/KemiaiElem.java
@@ -0,0 +1,44 @@
+
+package kemia;
+
+public class KemiaiElem {
+    private String ev;
+    private String elem;
+    private String vegyjel;
+    private int rendszam;
+    private String felfedezo;
+
+    @Override
+    public String toString() {
+        return "KemiaiElem{" + "ev=" + ev + ", elem=" + elem + ", vegyjel=" + vegyjel + ", rendszam=" + rendszam + ", felfedezo=" + felfedezo + '}';
+    }
+
+    public String getEv() {
+        return ev;
+    }
+
+    public String getElem() {
+        return elem;
+    }
+
+    public String getVegyjel() {
+        return vegyjel;
+    }
+
+    public int getRendszam() {
+        return rendszam;
+    }
+
+    public String getFelfedezo() {
+        return felfedezo;
+    }
+
+    public KemiaiElem(String sor) {
+        String [] s= sor.split(";");
+        this.ev = s[0];
+        this.elem = s[1];
+        this.vegyjel = s[2];
+        this.rendszam = Integer.parseInt(s[3]);
+        this.felfedezo = s[4];
+    }
+}
